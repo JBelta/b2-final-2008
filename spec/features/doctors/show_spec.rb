80b74407bf4e4241985c56fa1814baf5190809bd
James Belta <james.belta@gmail.com>
2020-11-09 09:26:08 -0700

    Include hospitals name and list of doctors patients

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -6,12 +6,15 @@ describe "As a visitor" do
       @hospital = Hospital.create!(name:"Grey Sloan Memorial Hospital")
       @doctor = @hospitals.doctors.create!(name:"Meredith Grey", speciality:"General Surgery", university: "Harvard University" )
     end
-    it "I see all of that doctors information including name, speciality, university" do
+    it "I see all of that doctors information including name, speciality, university as well as their hospital and list of patients" do
       visit "/doctors/#{@doctor.id}"
 
       ecpect(page).to have_content(@doctor.name)
       ecpect(page).to have_content(@doctor.speciality)
       ecpect(page).to have_content(@doctor.university)
+      expect(page).to have_content(@hospital.name)
+      expect(page).to have_content(@patient_1.name)
+      expect(page).to have_content(@patient_2.name)
     end
   end
 end
