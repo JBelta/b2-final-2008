98456abaf6cb07ea6723833f330cc324d1d12301
James Belta <james.belta@gmail.com>
2020-11-09 09:48:43 -0700

    spelling and syntax

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -5,15 +5,17 @@ describe "As a visitor" do
     before :each do
       @hospital = Hospital.create!(name:"Grey Sloan Memorial Hospital")
       @doctor = @hospital.doctors.create!(name:"Meredith Grey", speciality:"General Surgery", university: "Harvard University" )
-      @patient_1 = @doctor.patients.create!(name:"Katie Bryce", age:24 )
-      @patient_2 = @doctor.patients.create!(name:"Denny Duquette", age:39 )
+      @patient_1 = Patient.create!(name:"Katie Bryce", age:24 )
+      @patient_2 = Patient.create!(name:"Denny Duquette", age:39 )
+      DoctorPatient.create!(doctor_id: @doctor.id, patient_id: @patient_1.id)
+      DoctorPatient.create!(doctor_id: @doctor.id, patient_id: @patient_2.id)
     end
     it "I see all of that doctors information including name, speciality, university as well as their hospital and list of patients" do
       visit "/doctors/#{@doctor.id}"
 
-      ecpect(page).to have_content(@doctor.name)
-      ecpect(page).to have_content(@doctor.speciality)
-      ecpect(page).to have_content(@doctor.university)
+      expect(page).to have_content(@doctor.name)
+      expect(page).to have_content(@doctor.speciality)
+      expect(page).to have_content(@doctor.university)
       expect(page).to have_content(@hospital.name)
       expect(page).to have_content(@patient_1.name)
       expect(page).to have_content(@patient_2.name)
