6942ad9a0f8b5304d6d9ed0b09c649b4b545bce1
James Belta <james.belta@gmail.com>
2020-11-09 11:46:18 -0700

    cleaning up

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
 # Doctors
   get "/doctors/:doctor_id", to: 'doctors#show'
   delete '/doctors/:doctor_id/patients/:patient_id', to: 'patients#destroy'
+
 # Hospitals
   get '/hospitals/:hospital_id', to: 'hospitals#show'
 
 # Patients
-  #delete '/doctors/:doctor_id/patients/:patient_id', to: 'patients#destroy'
-
 end
