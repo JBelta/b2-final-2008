d691cd6f4357bd58435a4de78d53942245d06c0b
James Belta <james.belta@gmail.com>
2020-11-09 10:40:05 -0700

    edit test to include css id and show buttons for patients

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -17,8 +17,23 @@ describe "As a visitor" do
       expect(page).to have_content(@doctor.speciality)
       expect(page).to have_content(@doctor.university)
       expect(page).to have_content(@hospital.name)
-      expect(page).to have_content(@patient_1.name)
-      expect(page).to have_content(@patient_2.name)
+
+      within "#patient-#{@patient_1.id}" do
+        expect(page).to have_content(@patient_1.name)
+      end
+      within "#patient-#{@patient_2.id}" do
+        expect(page).to have_content(@patient_2.name)
+      end
     end
+    it "Next to each patient's name, I see a button to remove that patient from that doctors caseload." do
+      visit "/doctors/#{@doctor.id}"
+
+      within "#patient-#{@patient_1.id}" do
+        expect(page).to have_button("Remove")
+      end
+      within "#patient-#{@patient_2.id}" do
+        expect(page).to have_button("Remove")
+      end
+    end 
   end
 end
