2f3ab9df94c4c2fa099f4e191f106c79ba1b10ab
James Belta <james.belta@gmail.com>
2020-11-09 11:27:55 -0700

    Find patient/ doctor relationship and delete

--- app/models/doctor_patient.rb
+++ app/models/doctor_patient.rb
@@ -1,4 +1,9 @@
 class DoctorPatient < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  def self.remove(doctor, patient)
+    patient = where(doctor_id: doctor, patient_id: patient).first
+    patient.delete
+  end
 end
